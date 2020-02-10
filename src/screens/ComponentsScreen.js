7cadaa0393537f68b4f6e58de875a1bf9664861f
AJ Keeter <aleckeeter94@gmail.com>
2020-02-10 11:39:53 -0500

    Adds friends and custom functions for list screen

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text style={styles.textStyle}> This is the components screen </Text>;
+    const firstGreeting = 'Getting started with React Native!';
+    const name = 'Alec Keeter';
+
+    return (
+    <View>
+        <Text style={styles.textStyle}>{firstGreeting}</Text>
+        <Text style={styles.textStyle2}>My name is {name}</Text>
+    </View>
+    );
 };
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
+    },
+    textStyle2: {
+        fontSize: 20
     }
 });
 export default ComponentsScreen;
